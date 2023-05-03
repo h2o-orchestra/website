16405a386bcf33543cb137ba11b7230cc5247987
Xavier Gourmandin <xavier.gourmandin@stack-labs.com>
2023-05-03 16:45:37 +0200

    fix: try to fix Auth0 protection

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -10,16 +10,12 @@ const onRedirectCallback = (appState) => {
 };
 
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({ element }) => {
-    let redirectUri = window.location.origin;
-    if (window.location.pathname.length > 1) {
-        redirectUri += window.location.pathname
-    }
     return (
         <Auth0Provider
             domain={process.env.AUTH0_DOMAIN}
             clientId={process.env.AUTH0_CLIENTID}
             authorizationParams={{
-                redirect_uri: redirectUri,
+                redirect_uri: window.location.href,
             }}
             onRedirectCallback={onRedirectCallback}
         >
